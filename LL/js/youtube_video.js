22d77eacbc24c7a8684b024f88cc68258ecaa5c5
Charlie Zannorman <ccvannorman@gmail.com>
2017-12-16 11:00:48 -0800

    fixed issue where owl plugin was duplicating youtube players. redid owl carousel to look and feel right.

--- LL/js/youtube_video.js
+++ LL/js/youtube_video.js
@@ -48,6 +48,7 @@ function StopAllVideos(){
 	player2.stopVideo();
 	player3.stopVideo();
 	player4.stopVideo();
+	console.log('stopped vids. player1,2,3,4 state:'+player1.getPlayerState()+','+player2.getPlayerState()+','+player3.getPlayerState()+','+player4.getPlayerState()+' ..');
 }
 
 offset = 0;
@@ -62,7 +63,6 @@ function Resize(){
 	if (offset > 860) {
 		offY = "-" + parseInt((offset - 860)/5) + "px";
 	}
-	console.log('off:'+offY);
 
 // if offset width is at 860, we should keep yoffset 0 and adjust the width of iframe to become larger
 	// iff offset width is at 1170, the top offset should be about -100
@@ -73,3 +73,64 @@ function Resize(){
 $(document).ready(function(){
 	Resize();
 });
+
+var sliding = false;
+var rubberBumpDist = 25;
+var slideDuration = 350;
+function NextVideo(){
+	if (sliding) return;
+	var slideAmount = $('.episodes-carousel .item').first().width();
+	sliding = true;
+	wid = $(window).width();
+	all_children_width = 0;
+	$('.episodes-carousel .item').each(function(){ 
+		all_children_width += $(this).width();
+	});
+
+	var minL = wid - all_children_width;
+	
+	prevL = parseInt($('.episodes-carousel').css('left'));
+	prevL -= slideAmount;
+	
+	if (prevL < minL){
+		prevL = minL - rubberBumpDist;
+		var leftDelta = parseInt($('.episodes-carousel').css('left'))-prevL;
+		leftDelta = Math.min(slideDuration,leftDelta);
+		console.log('bumpdist:'+leftDelta);
+		if (leftDelta == rubberBumpDist){
+
+			$('.episodes-carousel').animate({'left':prevL+"px"},100).animate({'left':minL},slideDuration,function(){sliding=false;});
+		} else {
+			$('.episodes-carousel').animate({'left':minL},leftDelta,function(){sliding=false;});
+		
+		}
+	
+	} else {
+		$('.episodes-carousel').animate({'left':prevL+"px"},slideDuration,function(){sliding=false});
+	
+	}
+}
+
+function PreviousVideo(){
+	if (sliding) return;
+	prevL = parseInt($('.episodes-carousel').css('left'));
+	var slideAmount = $('.episodes-carousel .item').first().width();
+	prevL += slideAmount;
+	
+	if (prevL > 0){
+		prevL = rubberBumpDist;
+		var leftDelta = prevL - parseInt($('.episodes-carousel').css('left'));
+		leftDelta = Math.min(slideDuration,leftDelta);
+		if (leftDelta == rubberBumpDist){	
+			$('.episodes-carousel').animate({'left':prevL+"px"},100).animate({'left':0},slideDuration,function(){sliding=false});
+		} else {
+			$('.episodes-carousel').animate({'left':0},leftDelta,function(){sliding=false});
+		
+		}
+		console.log('bumpdist:'+leftDelta);
+	
+	} else {
+		$('.episodes-carousel').animate({'left':prevL+"px"},slideDuration,function(){sliding=false});
+	
+	}
+}
