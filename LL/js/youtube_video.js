37665dd487ef7399fa96d1d859cf15b3be6a240d
Charlie Zannorman <ccvannorman@gmail.com>
2017-12-15 19:00:54 -0800

    no video preview but site is almost there.

--- LL/js/youtube_video.js
+++ LL/js/youtube_video.js
@@ -26,7 +26,48 @@ function onPlayerReady(eventData){}
 
 function onPlayerStateChange(eventData){}
 
-function PlayThisVideo(player,el){
+function PlayVideo(player,el){
+	StopAllVideos();
+	var regVidHeight = 238;
+
+	$('.episodes-carousel .item').each(function(){
+		$(this).find('.videoWrapper').hide();
+		$(this).find('.img').css('height',regVidHeight);
+		$(this).find('.play-episode').show();
+	});
+	$(el).parent().find('.play-episode').hide();
+	$(el).parent().find('.videoWrapper').show();
+	$(el).parent().find('.img').css('height','12px');
+	console.log('el:'+$(el).attr('class')+", el par:"+$(el).parent().attr('class'));
+	player.playVideo();
 	
 }
 
+function StopAllVideos(){
+	player1.stopVideo();
+	player2.stopVideo();
+}
+
+offset = 0;
+$(window).resize(function(){
+	Resize();
+});
+
+	
+function Resize(){
+	offset = parseInt($('.mainVid').css('width').replace('px',''));
+	var offY = 0;
+	if (offset > 860) {
+		offY = "-" + parseInt((offset - 860)/5) + "px";
+	}
+	console.log('off:'+offY);
+
+// if offset width is at 860, we should keep yoffset 0 and adjust the width of iframe to become larger
+	// iff offset width is at 1170, the top offset should be about -100
+
+	$('.mainVid iframe').css('top',offY);
+}
+
+$(document).ready(function(){
+	Resize();
+});
